329ffec9def3bbf7dbc566c58cbaa87ba77211b7
Zheng <yuanzhengstl@gmail.com>
2018-02-28 10:23:02 -0800

    update the controller to be open for extension

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -14,10 +14,10 @@ module ShoutHelper
   end
 
   def shout_form_for(content_type)
-    form_for Shout.new do |form|
-      form.hidden_field(:content_type, value: "content_type") +
-      form.fields_for(:content) { |content_form| yield(content_form)} +
-      form.submit("Shout!")
+    form_for(Shout.new, url: content_type.new) do |form|
+      form.hidden_field(:content_type, value: content_type)+
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit('Shout!')
     end
   end
 end
